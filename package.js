bbbb162460c7433f6ab5552c746cfa9aa43cc658
Dominik Guzei <dominik.guzei@gmail.com>
2015-10-15 23:56:04 +0200

    bumps version to 1.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Helpers for Testing Space Applications and Modules.',
   name: 'space:testing',
-  version: '1.4.4',
+  version: '1.5.0',
   git: 'https://github.com/meteor-space/testing.git',
   debugOnly: true
 });
