268c507de4a48524543b3e2a05f1650d1f8ebc9e
qejk <qejk@qejk.net>
2017-01-26 19:11:23 +0100

    Exports serializable, versionable custom chai assertions, moves spaceChai to index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import isSubclassOf from './helpers/is-subclass.js';
 import same from './assertions/same.js';
+import serializable from './assertions/serializable.js';
+import versionable from './assertions/versionable.js';
 import extend from './assertions/extend.js';
 import dependOn from './assertions/depend-on.js';
 import containArrayOfStructs from './assertions/contain-array-of-structs.js';
 import matchArrayOfStructs from './assertions/match-array-of-structs.js';
-import spaceChai from './assertions/space-chai';
 
 const extensions = {};
 extensions.chai = {
   same: same,
+  serializable: serializable,
+  versionable: versionable,
   extend: extend,
   dependOn: dependOn,
   containArrayOfStructs: containArrayOfStructs,
   matchArrayOfStructs: matchArrayOfStructs
+};
+
+const spaceChai = function(chai) {
+  for (let extension of extensions.chai) {
+    chai.use(extension);
+  }
 };
 
 export {
